165f52194a27aa0d3592b4d4185c11fbc66289b2
gderuwez <gderuwez@gmail.com>
2018-05-30 14:26:43 +0200

    menu changed to collapse

--- fragment/Carte.php
+++ fragment/Carte.php
@@ -5,46 +5,66 @@
 <div class="container mt-2 bg-creme">
   <div class="row">
     <div class="col-md-6">
-      <h1 class="font-cursive">Entrance</h1>
-      <ul class="list-group">
-        <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">10</span></li>
-        <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">20</span></li>
-        <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">15</span></li>
-        <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">25</span></li>
-      </ul>
+      <div class="row">
+        <a class="font-cursive h1 col-12" href="#entrance" data-toggle="collapse">Entrance</a>
+      </div>
+      <div class="row">
+        <ul id="entrance" class="list-group collapse col-12">
+          <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">10</span></li>
+          <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">20</span></li>
+          <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">15</span></li>
+          <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">25</span></li>
+        </ul>
+      </div>
+      <div class="row">
+        <a class="font-cursive mt-3 h1 col-12" href="#main" data-toggle="collapse">Main courses</a>
+      </div>
+      <div class="row">
+        <ul class="list-group collapse col-12" id="main">
+          <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">30</span></li>
+          <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">45</span></li>
+          <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">40</span></li>
+          <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">38</span></li>
+        </ul>
+      </div>
 
-      <h1 class="font-cursive mt-3">Main courses</h1>
-      <ul class="list-group">
-        <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">30</span></li>
-        <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">45</span></li>
-        <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">40</span></li>
-        <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">38</span></li>
-      </ul>
-
-      <h1 class="font-cursive mt-3">Desserts</h1>
-      <ul class="list-group">
-        <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">8</span></li>
-        <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">12</span></li>
-        <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">9</span></li>
-        <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">15</span></li>
-      </ul>
+      <div class="row">
+        <a class="font-cursive mt-3 h1 col-12" href="#desserts" data-toggle="collapse">Desserts</a>
+      </div>
+      <div class="row">
+        <ul class="list-group collapse col-12" id="desserts">
+          <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">8</span></li>
+          <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">12</span></li>
+          <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">9</span></li>
+          <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">15</span></li>
+        </ul>
+      </div>
     </div>
-    <div class="col-md-6">
-      <h1 class="font-cursive mt-md-0 mt-3">Snacks</h1>
-      <ul class="list-group">
-        <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">3</span></li>
-        <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">2</span></li>
-        <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">2.5</span></li>
-        <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">1.45</span></li>
-      </ul>
 
-      <h1 class="font-cursive mt-3">Drinks</h1>
-      <ul class="list-group">
-        <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">2</span></li>
-        <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">2.3</span></li>
-        <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">3</span></li>
-        <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">1.7</span></li>
-      </ul>
+    <div class="col-md-6">
+      <div class="row">
+        <a class="font-cursive mt-md-0 mt-3 h1 col-12" href="#snacks" data-toggle="collapse">Snacks</a>
+      </div>
+      <div class="row">
+        <ul class="list-group collapse col-12" id="snacks">
+          <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">3</span></li>
+          <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">2</span></li>
+          <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">2.5</span></li>
+          <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">1.45</span></li>
+        </ul>
+      </div>
+
+      <div class="row">
+        <a class="font-cursive mt-3 h1 col-12" href="#drinks" data-toggle="collapse">Drinks</a>
+      </div>
+      <div class="row">
+        <ul class="list-group collapse col-12" id="drinks">
+          <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">2</span></li>
+          <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">2.3</span></li>
+          <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">3</span></li>
+          <li class="list-group-item border-0">Lorem Ipsum<span class="badge badge-light float-right">1.7</span></li>
+        </ul>
+      </div>
     </div>
   </div>
 </div>
